2da6086264d1c2c554b6929903ae71a898c49c4f
ruijadom <ruijadomingues@gmail.com>
2023-09-08 23:30:28 +0200

    refactor(routes): create home route

---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import App from "./App.tsx";
+import "./index.css";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <App />
+      </Router>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import { useRoutes } from "react-router-dom";
+
+import HomePage from "@/pages/home.page";
+
+function Routes() {
+  const element = useRoutes([
+    {
+      path: "/",
+      element: <HomePage />
+    },
+  ]);
+
+  return element;
+}
+
+export default Routes;
