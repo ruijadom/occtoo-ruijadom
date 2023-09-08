f21f1622d2f65970b36074fbb88c4bce06358475
ruijadom <ruijadomingues@gmail.com>
2023-09-08 23:32:31 +0200

    feat(infite-loader): component for create a infinite loader

---
+++ src/components/infinite-loader.tsx
@@ -0,0 +1,108 @@
+import React, { HTMLAttributes, useState } from "react";
+import useIntersectionObserver from "@/hooks/useIntersectionObserver";
+
+/**
+ * Props for the InfiniteLoader component.
+ * @template T - The type of items loaded by the component.
+ */
+interface InfiniteLoaderProps<T> {
+  /**
+   * A function to fetch more items.
+   * @param skip - The number of items to skip.
+   * @param limit - The number of items to fetch.
+   * @returns A Promise that resolves to an array of items of type T.
+   */
+  fetchFn: (skip?: number, limit?: number) => Promise<T[]>;
+
+  /**
+   * A boolean indicating whether there are more pages to load.
+   */
+  hasNextPage: boolean;
+
+  /**
+   * A function to render the loaded items.
+   * @param items - An array of items of type T to render.
+   * @returns A React node representing the rendered items.
+   */
+  renderItems: (items: T[]) => React.ReactNode;
+
+  /**
+   * (Optional) A React node to display when there are no more items to load.
+   */
+  noMoreItems?: React.ReactNode;
+
+  /**
+   * (Optional) HTML attributes for the `<ul>` element.
+   */
+  ulNativeProps?: HTMLAttributes<HTMLUListElement>;
+
+  /**
+   * (Optional) HTML attributes for the `<li>` elements.
+   */
+  liNativeProps?: HTMLAttributes<HTMLLIElement>;
+}
+
+/**
+ * A component for infinite scrolling and loading more items.
+ * @template T - The type of items loaded by the component.
+ * @param {InfiniteLoaderProps<T>} props - The component's props.
+ */
+function InfiniteLoader<T>({
+  fetchFn,
+  hasNextPage,
+  renderItems,
+  noMoreItems,
+  ulNativeProps,
+  liNativeProps
+}: InfiniteLoaderProps<T>): React.ReactElement {
+  const [items, setItems] = useState<T[]>([]);
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+
+  /**
+   * Fetch more items asynchronously.
+   * @param skip - The number of items to skip.
+   * @returns A Promise that resolves to an array of new items.
+   */
+  const fetchMoreItems = async (skip: number) => {
+    if (isFetching) return;
+
+    setIsFetching(true);
+
+    try {
+      const newItems = await fetchFn(skip);
+      return newItems;
+    } catch (error) {
+      console.error("Error fetching more data:", error);
+    } finally {
+      setIsFetching(false);
+    }
+  };
+
+  /**
+   * A reference to the last product element in the list, used for intersection observing.
+   */
+
+  const lastProductRef = useIntersectionObserver<HTMLDivElement>(() => {
+    void fetchMoreItems(items.length).then(
+      (newItems) => newItems && setItems((items) => [...items, ...newItems])
+    );
+  }, [hasNextPage, !isFetching]);
+
+  return (
+    <>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 md:gap-6" {...ulNativeProps}>
+        {React.Children.toArray(renderItems(items)).map((item, index) => {
+          return (
+            <li key={index} {...liNativeProps}>
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+      <div ref={lastProductRef}></div>
+      {!hasNextPage && noMoreItems}
+    </>
+  );
+}
+
+export default InfiniteLoader;
---
+++ src/pages/home.page.tsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+
+import InfiniteLoader from "@/components/infinite-loader";
+import dummyApi from "@/services/dummy-api";
+
+export interface ProductResponse {
+  products: Product[];
+  total: number;
+  skip: number;
+  limit: number;
+}
+
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+}
+
+function App() {
+  const [hasNextPage, setHasNextPage] = useState(true);
+
+  const fetchApi = async (skip = 0, limit = 9) => {
+    const response = await dummyApi.get(
+      `/products?limit=${limit}&skip=${skip}`
+    );
+
+    const data: ProductResponse = response.data;
+
+    if (data.total <= skip + limit) setHasNextPage(false);
+
+    return data.products;
+  };
+
+  const renderElement = (items: Product[]) => {
+    return items.map((product: Product) => (
+      <div key={product.id} className="relative border h-60">
+        <img
+          className="flex h-full w-full object-cover"
+          src={product.thumbnail}
+          alt={product.title}
+        />
+        <div className="absolute bottom-0 px-3 py-2 w-full bg-black opacity-80 text-white capitalize">
+          <h2 className=" font-semibold text-md truncate">{product.title}</h2>
+          <p className=" font-extralight text-sm">{product.brand}</p>
+        </div>
+      </div>
+    ));
+  };
+
+  const noMoreItemsElement = <p className="text-center">No more items</p>;
+
+  return (
+    <div className="min-h-screen w-full">
+      <div className="px-4 py-8 max-w-6xl mx-auto">
+        <h2 className="text-2xl font-bold mb-4">Custom Loader</h2>
+        <InfiniteLoader
+          fetchFn={fetchApi}
+          hasNextPage={hasNextPage}
+          renderItems={renderElement}
+          noMoreItems={noMoreItemsElement}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
